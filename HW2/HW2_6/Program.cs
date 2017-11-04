c5e71c7606094de8c39dcf81cfcb2dd1f28a36b1
Dmitriy Samsonov (DS1) <pichu-pikachu@mail.ru>
2017-11-04 22:52:34 +0300

    HW1_6: Add comments; HW2: Task 6.

--- HW2/HW2_6/Program.cs
+++ HW2/HW2_6/Program.cs
@@ -1,7 +1,8 @@
-﻿//6.* Создать класс методами, которые​могут пригодиться в вашей 
-//учебе(Print, Pause).
+﻿// 6. *Написать программу подсчета количества “Хороших” чисел в диапазоне от 1
+// до 1 000 000 000. Хорошим называется число, которое делится на сумму своих
+// цифр. Реализовать подсчет времени выполнения программы, используя структуру
+// DateTime.
 // Samsonov
-
 
 using System;
 using System.Collections.Generic;
@@ -9,10 +10,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-/// <summary>
-/// 
-/// </summary>
-namespace HW1_6
+namespace HW2_6
 {
     /// <summary>
     /// Класс полезных функций
@@ -79,11 +77,52 @@ namespace HW1_6
     class Program
     {
         /// <summary>
+        /// Функция подсчёта суммы цифр в числе
+        /// </summary>
+        /// <param name="number">Исходное число</param>
+        /// <returns>Сумма чисел исходного числа</returns>
+        static int SumOfNumeral(int number)
+        {
+            int sum = 0;
+            for (; number > 0; number /= 10)
+                sum += number % 10;
+            return sum;
+        }
+
+        /// <summary>
+        /// Является ли данное число "хорошим"?
+        /// </summary>
+        /// <param name="number">Данное число</param>
+        /// <returns>Возращает true, если число хорошее, иначе false</returns>
+        static bool IsGoodNumber(int number)
+        {
+            if (number % SumOfNumeral(number) == 0)
+                return true;
+            return false;
+        }
+
+        /// <summary>
         /// Точка входа
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            var specFunc = new FunctionsForStudy();
+            var startTime = DateTime.Now;
+            int cnt = 0;
+
+            for (int i = 1; i <= 1000000000; i++)
+            {
+                if (IsGoodNumber(i))
+                    cnt++;
+                if (i % 1000000 == 0)
+                    Console.Write(string.Format("Проверено: {0}\r", i));
+            }
+
+            var endTime = DateTime.Now;
+
+            Console.WriteLine(string.Format("Количество хороших чисел: {0}\nВремя выполнения: {1}", cnt, endTime - startTime));
+            specFunc.Pause();
         }
     }
 }
