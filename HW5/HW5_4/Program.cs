717c52086cd22e31b3e0f1db0fe649b9146597d8
Dmitriy Samsonov (DS1) <pichu-pikachu@mail.ru>
2017-11-17 20:10:56 +0300

    HW5: add task 4.

---
+++ HW5/HW5_4/Program.cs
@@ -0,0 +1,66 @@
+﻿// 4. *Задача ЕГЭ.
+// На вход программе подаются сведения о сдаче экзаменов учениками 9-х классов
+// некоторой средней школы.В первой строке сообщается количество учеников N,
+// которое не меньше 10, но не превосходит 100, каждая из следующих N строк имеет
+// следующий формат:
+// <Фамилия> <Имя> <оценки>,
+// где <Фамилия> – строка, состоящая не более чем из 20 символов, <Имя> –
+// строка, состоящая не более чем из 15 символов, <оценки> – через пробел три
+// целых числа, соответствующие оценкам по пятибалльной системе. <Фамилия> и
+// <Имя>, а также <Имя> и <оценки> разделены одним пробелом. Пример входной
+// строки:
+// Иванов Петр 4 5 3
+// Требуется написать как можно более эффективную программу, которая будет
+// выводить на экран фамилии и имена трех худших по среднему баллу учеников.
+// Если среди остальных есть ученики, набравшие тот же средний балл, что и один
+// из трех худших, то следует вывести и их фамилии и имена.
+// Samsonov
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilityForStudyNameSpace;
+
+namespace HW5_4
+{
+    class Program
+    {
+        /// <summary>
+        /// Точка входа
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            var specFunc = new UtilityForStudy();
+
+            int n = int.Parse(Console.ReadLine());
+            Student[] stds = new Student[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Student std = new Student(Console.ReadLine().Split(' '));
+                int j = i - 1;
+                for (j = i - 1; j >= 0; j--)
+                    if (stds[j].AverageMark > std.AverageMark)
+                        stds[j + 1] = stds[j];
+                    else
+                        break;
+                stds[j + 1] = std;
+            }
+
+            int cnt = 3, k = 0;
+            do
+            {
+                Console.WriteLine(stds[k]);
+                k++;
+                cnt--;
+            }
+            while (cnt > 0 || stds[k].AverageMark == stds[k - 1].AverageMark);
+
+
+            specFunc.Pause();
+        }
+    }
+}
---
+++ HW5/HW5_4/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW5_4
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    class Student
+    {
+        /// <summary>
+        /// Средняя оценка
+        /// </summary>
+        private double averageMark;
+
+        /// <summary>
+        /// Строки имени и фамилии
+        /// </summary>
+        private string name, surname;
+
+        /// <summary>
+        /// Свойство средней оценки
+        /// </summary>
+        public double AverageMark
+        {
+            get { return averageMark; }
+        }
+
+        /// <summary>
+        /// Конструктор от массива строк
+        /// </summary>
+        /// <param name="info">Массив строк</param>
+        public Student(string[] info)
+        {
+            surname = info[0];
+            name = info[1];
+            averageMark = (int.Parse(info[2]) + int.Parse(info[3]) + int.Parse(info[4])) / 3.0;
+        }
+
+        /// <summary>
+        /// Конструктор от элементов
+        /// </summary>
+        /// <param name="Surname">Фамилия</param>
+        /// <param name="Name">Имя</param>
+        /// <param name="Mark1">1-ая оценка</param>
+        /// <param name="Mark2">2-ая оценка</param>
+        /// <param name="Mark3">3-ья оценка</param>
+        public Student(string Surname, string Name, int Mark1, int Mark2, int Mark3)
+        {
+            name = Name;
+            surname = Surname;
+            averageMark = (Mark1 + Mark2 + Mark3) / 3.0;
+        }
+
+        /// <summary>
+        /// Перегрузка метода для класса студента в строку.
+        /// </summary>
+        /// <returns>Строку</returns>
+        override public string ToString()
+        {
+            string str = string.Format("{0} {1}", surname, name);
+            return str;
+        }
+    }
+}
