14703c2a851eb53d8b50f53d6a187cd3f8c0ad47
Dmitriy Samsonov (DS1) <pichu-pikachu@mail.ru>
2017-11-28 12:57:17 +0300

    HW8: initial task 2;

---
+++ HW8/HW8_2/Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace HW8_2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(640, 480);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(640, 480);
+            this.Name = "MainForm";
+            this.Text = "Верю Не верю";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
